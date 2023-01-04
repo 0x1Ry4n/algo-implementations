0bb11b894b310176da87cb717b120201bf93bed7
Ryan <ryanlucas2018gz@gmail.com>
2023-01-04 00:51:17 -0300

    Revisão cinco mais um teste!

--- hello.c
+++ hello.c
@@ -2,10 +2,6 @@
 
 int main()
 {
-<<<<<<< HEAD
-    printf("Hello World branch cinco!");
-=======
-    printf("Hello World, testandoooooo!");
->>>>>>> eb45ed0cfb7d79ec52edd87fd8bb5ecbb350d1b1
+    printf("Hello World branch cinco mais um testezinho!");
     return 0;
 }
